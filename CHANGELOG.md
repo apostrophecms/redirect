07daa53fcfb909a5f492b12213c6f46c0c8e05ec
Stuart Romanek <stuart@apostrophecms.com>
2021-06-21 12:50:32 -0400

    initial

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 # Changelog
 
 ## 1.0.0
+Initial port from Apostrophe 2.0
--- index.js
+++ index.js
@@ -1,20 +1,175 @@
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
+  extend: '@apostrophecms/piece-type',
   options: {
-    alias: 'ALIAS',
+    alias: 'redirect',
+    label: 'Redirect',
+    pluralLabel: 'Redirects',
+    searchable: false,
+    editRole: 'editor',
+    publishRole: 'editor',
+    withType: '@apostrophecms/page',
+    autopublish: true,
+    quickCreate: false,
+    statusCode: 302,
+    openGraph: false, // Disables @apostrophecms/open-graph for redirects
+    seo: false, // Disables @apostrophecms/seo for redirects
+    sitemap: false // Disables @apostrophecms/sitemap for redirects
   },
-  bundle: {
-    directory: 'modules',
-    modules: getBundleModuleNames()
+  // init(self) {
+  //   // console.log(self.apos);
+  //   console.log(Object.keys(self.apos.modules));
+  //   if (!self.apos.modules[self.options.withType]) {
+  //     console.warn(`Redirects can not create relationships to ${self.options.withType}, defaulting to @apostrophecms/page`)
+  //   }
+  // },
+  handlers(self) {
+    return {
+      beforeSave: {
+        slugPrefix(req, doc) {
+          doc.slug = 'redirect-' + doc.redirectSlug;
+
+          if (!doc.title) {
+            doc.title = doc.redirectSlug;
+          }
+        }
+      }
+    };
+  },
+  fields(self, options) {
+    const remove = [
+      'visibility',
+    ];
+    const add = {
+      redirectSlug: {
+        // This is *not* type: 'slug' because we want to let you match any
+        // goldang nonsense the old site had in there, including mixed case
+        type: 'string',
+        label: 'Old URL',
+        help: 'Format with leading / such as /old-url',
+        required: true
+      },
+      title: {
+        label: 'Description',
+        type: 'string',
+        required: true
+      },
+      urlType: {
+        label: 'Link To',
+        type: 'select',
+        choices: [
+          { label: 'Internal Page', value: 'internal' },
+          { label: 'External URL', value: 'external' }
+        ],
+        def: 'internal'
+      },
+      ignoreQueryString: {
+        label: 'Ignore query string when matching.',
+        type: 'boolean',
+        def: false
+      },
+      _newPage: {
+        type: 'relationship',
+        label: 'Page Title',
+        withType: '@apostrophecms/page',
+        if: {
+          urlType: 'internal',
+        },
+        builders: {
+          // Editors+ set up redirects, so it's OK for non-admins to follow them anywhere
+          // (they won't actually get access without logging in)
+          project: {
+            slug: 1,
+            title: 1,
+            _url: 1
+          }
+        },
+        max: 1
+      },
+      externalUrl: {
+        label: 'URL',
+        type: 'url',
+        if: {
+          urlType: 'external',
+        }
+      },
+      statusCode: {
+        label: 'Redirect Type',
+        type: 'radio',
+        htmlHelp: 'Use <strong>"Temporary"</strong> for temporary redirects or for testing purposes.<br />Use <strong>"Permanent"</strong> after testing, this is an SEO best practice.',
+        choices: [
+          {
+            label: 'Temporary (Status code 302)',
+            value: '302'
+          },
+          {
+            label: 'Permanent (Status code 301, Search engines will cache these, proofread carefully)',
+            value: '301'
+          }
+        ],
+        def: '302'
+      }
+    }
+
+    const group = {
+      basics: {
+        label: 'Basics',
+        fields: [
+          'title',
+          'redirectSlug',
+          'urlType',
+          '_newPage',
+          'externalUrl',
+          'statusCode',
+          'ignoreQueryString'
+        ]
+      }
+    }
+
+    if (options.statusCode.toString() === '301') {
+      add.statusCode.def = options.statusCode.toString();
+    }
+
+    add._newPage.withType = self.options.withType;
+
+    return {
+      add,
+      remove,
+      group
+    };
+  },
+  middleware(self, options) {
+    return {
+      async checkRedirect(req, res, next) {
+        let slug = req.url;
+        let pathOnly = slug.split('?')[0];
+        let redirectRegEx = new RegExp(`^redirect-${self.apos.util.regExpQuote(pathOnly)}(\\?.*)?$`);
+        let results = await self.find(req, { slug: redirectRegEx }).toArray();
+        let target;
+        if (results) {
+          if (results.some(result => result.redirectSlug == slug)) {
+            target = results.find(result => result.redirectSlug == slug);
+          } else if (results.some(result => result.redirectSlug == pathOnly && result.ignoreQueryString)) {
+            target = results.find(result => result.redirectSlug == pathOnly && result.ignoreQueryString);
+          }
+
+          if (target) {
+            let status = parseInt(target.statusCode);
+
+            if (isNaN(status) || !status) {
+              status = 302;
+            }
+
+            if (target.urlType === 'internal' && target._newPage && target._newPage[0]) {
+              return req.res.redirect(status, target._newPage[0]._url);
+            } else if (target.urlType === 'external' && target.externalUrl.length) {
+              return req.res.redirect(status, target.externalUrl);
+            } else {
+              return next();
+            }
+          }
+        }
+        return next();
+      }
+    };
   }
 };
-
-function getBundleModuleNames() {
-  const source = path.join(__dirname, './modules/@apostrophecms');
-  return fs
-    .readdirSync(source, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => `@apostrophecms/${dirent.name}`);
-}
