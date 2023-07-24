8b4700709bc4feb4078cf5d8a459c351cf8b9135
Jed <vjeudy@protonmail.com>
2023-07-24 17:49:24 +0200

    improving code, checking originalUrl (with locale prefix) and redirecting to redirectSlug and not page url

--- index.js
+++ index.js
@@ -146,34 +146,30 @@ module.exports = {
     return {
       async checkRedirect(req, res, next) {
         try {
-          const slug = req.url;
+          const slug = req.originalUrl;
           const pathOnly = slug.split('?')[0];
           const redirectRegEx = new RegExp(`^redirect-${self.apos.util.regExpQuote(pathOnly)}(\\?.*)?$`);
           const results = await self.find(req, { slug: redirectRegEx }).toArray();
-          let target;
-          if (results) {
-            if (results.some(result => result.redirectSlug === slug)) {
-              target = results.find(result => result.redirectSlug === slug);
-            } else if (results.some(result => result.redirectSlug === pathOnly && result.ignoreQueryString)) {
-              target = results.find(result => result.redirectSlug === pathOnly && result.ignoreQueryString);
-            }
-
-            if (target) {
-              let status = parseInt(target.statusCode);
-
-              if (isNaN(status) || !status) {
-                status = 302;
-              }
-
-              if (target.urlType === 'internal' && target._newPage && target._newPage[0]) {
-                return req.res.redirect(status, target._newPage[0]._url);
-              } else if (target.urlType === 'external' && target.externalUrl.length) {
-                return req.res.redirect(status, target.externalUrl);
-              } else {
-                return await emitAndRedirectOrNext();
-              }
-            }
+          if (!results || !results.length) {
+            return await emitAndRedirectOrNext();
           }
+
+          const target = results.find(({ redirectSlug }) => redirectSlug === slug) ||
+           results.find(({ redirectSlug, ignoreQueryString }) => redirectSlug === pathOnly && ignoreQueryString);
+
+          if (!target) {
+            return await emitAndRedirectOrNext();
+          }
+
+          const parsedCode = parseInt(target.statusCode);
+          const status = (parsedCode && !isNaN(parsedCode)) ? parsedCode : 302;
+
+          if (target.urlType === 'internal' && target._newPage && target._newPage[0]) {
+            return req.res.redirect(status, target.redirectSlug);
+          } else if (target.urlType === 'external' && target.externalUrl.length) {
+            return req.res.redirect(status, target.externalUrl);
+          }
+
           return await emitAndRedirectOrNext();
         } catch (e) {
           self.apos.util.error(e);
